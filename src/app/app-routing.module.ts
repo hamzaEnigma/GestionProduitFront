86e2992a768586bc32c9d968d9aaa4a958d00488
Hamza Somai <hamza.somai@ced.group>
2023-09-29 14:51:00 +0100

    add the routing module and use the router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProductsComponent } from './Products/list-products/list-products.component';
+import { WelcomeComponent } from './Welcome/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ListProductsComponent },
+  { path: 'welcome', component: WelcomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
